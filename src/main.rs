f3bd1aeef2237fa8c27283d8464fe2cd8ff3fd35
Hyrum Wright <hyrum@hyrumwright.org>
2018-02-19 23:18:28 -0500

    Parse configuration file

--- src/main.rs
+++ src/main.rs
@@ -4,28 +4,48 @@
 extern crate clap;
 use clap::{App, Arg};
 
-extern crate env_logger;
 #[macro_use]
-extern crate log;
+extern crate serde_derive;
+extern crate serde_yaml;
+
+extern crate prometheus;
+
+mod config {
+    use std::fs::File;
+
+    use serde_yaml;
+
+    #[derive(Debug, Deserialize)]
+    pub struct Config {
+        server: Server,
+    }
+
+    #[derive(Debug, Deserialize)]
+    pub struct Server {
+        port: u16,
+    }
+
+    impl Config {
+        pub fn new(filename: &str) -> Config {
+            serde_yaml::from_reader(File::open(filename).unwrap()).unwrap()
+        }
+    }
+}
 
 fn main() {
-    env_logger::init().unwrap();
-
     let matches = App::new("Prom Rain")
         .version("0.1.0")
         .author("Hyrum Wright <hyrum@hyrumwright.org>")
         .arg(
-            Arg::with_name("port")
-                .short("p")
-                .long("port")
-                .help("Which port to export metrics on")
-                .default_value("9090")
+            Arg::with_name("config")
+                .long("config")
+                .help("File to use for configuration")
+                .default_value("config.yml")
                 .takes_value(true),
         )
         .get_matches();
 
-    let port = matches.value_of("port").unwrap();
-    let port: u16 = port.parse().expect("Port value is not a valid integer");
-
-    info!("Port: {}", port);
+    let config = matches.value_of("config").unwrap();
+    let config = config::Config::new(config);
+    println!("Config: {:?}", config);
 }
