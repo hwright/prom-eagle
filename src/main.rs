cd24be478a57d58ec4c18e53234c634007bd2a0c
Hyrum Wright <hyrum@hyrumwright.org>
2018-02-24 09:03:06 -0500

    Output a simple prometheus metric on any request

--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,20 @@
 // prometheus metrics
 
 extern crate clap;
-use clap::{App, Arg};
-
+extern crate hyper;
+#[macro_use]
+extern crate lazy_static;
+#[macro_use]
+extern crate prometheus;
 #[macro_use]
 extern crate serde_derive;
 extern crate serde_yaml;
 
-extern crate hyper;
-extern crate prometheus;
-
+use clap::{App, Arg};
 use hyper::header::ContentType;
 use hyper::mime::Mime;
 use hyper::server::{Request, Response, Server};
+use prometheus::{Counter, Encoder, TextEncoder};
 
 use std::net;
 
@@ -39,6 +41,16 @@ mod config {
     }
 }
 
+lazy_static! {
+    static ref HTTP_COUNTER: Counter = register_counter!(
+        opts!(
+            "example_http_requests_total",
+            "Total number of HTTP requests made.",
+            labels!{"handler" => "all",}
+        )
+    ).unwrap();
+}
+
 fn main() {
     let matches = App::new("Prom Rain")
         .version("0.1.0")
@@ -56,10 +68,18 @@ fn main() {
     let config = config::Config::new(config);
     println!("Config: {:?}", config);
 
+    let encoder = TextEncoder::new();
     let addr = "0.0.0.0".parse().unwrap();
     let addr = net::SocketAddr::new(addr, config.server.port);
     Server::http(addr)
         .unwrap()
-        .handle(move |req: Request, res: Response| {})
+        .handle(move |req: Request, mut res: Response| {
+            let metric_families = prometheus::gather();
+            let mut buffer = vec![];
+            encoder.encode(&metric_families, &mut buffer).unwrap();
+            res.headers_mut()
+                .set(ContentType(encoder.format_type().parse::<Mime>().unwrap()));
+            res.send(&buffer).unwrap();
+        })
         .unwrap();
 }
